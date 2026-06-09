d425a27cf5e62f282d82372c106e9c2cb95b6d77
kanishkaindusara92-spec <kanishkaindusara92@gmail.com>
2026-06-09 14:40:50 +0530

    Upload website files

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+var CACHE_NAME = 'kn-box-v1';
+var ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(ASSETS); }));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys().then(function(keys) {
+        return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+    }));
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(fetch(event.request).then(function(response) {
+        if (response && response.status === 200 && response.type === 'basic') {
+            var clone = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, clone); });
+        }
+        return response;
+    }).catch(function() { return caches.match(event.request); }));
+});
